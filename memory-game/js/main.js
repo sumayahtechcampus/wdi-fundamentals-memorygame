133ac0381e4df5d8d1e6007e55633f024718360a
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-09-20 00:23:55 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,21 +1,44 @@
-const cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank: "queen",
+suit: "heaarts",
+cardImage:"images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank:"king",
+suit: "heaarts",
+cardImage:"images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamaonds",
+cardImage:"images/king-of-diamonds.png",
+},
+];
 const cardsInPlay = [];
 
 function checkForMatch(){
 if (cardsInPlay.length === 2) {
 
 if ( cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  akert("You found a match!");
 }
 
 else{
-  console.log("sorry try again");
+  alert("sorry try again");
 }
 }
 }
 function flipCard(cardId){
-  console.log("user flipped" + " " + cards[cardId])
-  cardsInPlay.push(cards[cardId]);
+  console.log("user flipped" + " " + cards[cardId].rank)
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 flipCard(0);
